2a55a8a601ea25b839f8c5fad4b8a683bcd0f81d
AntipinN <antipinko.nik@yandex.ru>
2024-12-07 20:01:32 +0300

    добавил десериализацию текста для презентации

---
+++ AvaloniaApplication3/Models/Text_for_presentation.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AvaloniaApplication3.Models
+{
+    public class Text_for_presentation
+    {
+        public List<string> text = new List<string>();
+        public Text_for_presentation() 
+        {
+            using (FileStream fs = new FileStream($"JSON_SERIALIZE.json", FileMode.OpenOrCreate))
+            {
+                text = System.Text.Json.JsonSerializer.Deserialize<List<string>>(fs);
+            }
+        }
+    }
+}
