13ca40e927908adb97e9b08b700055e56b64230b
Pantorius <flyspore@yandex.ru>
2024-12-23 23:31:13 +0300

    Refactoring. Model now has wrapper_fraction class that uses for equasion representation. Added error handler static class. Few UI changes.

---
+++ AvaloniaApplication3/ErrorHandlerSaverClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+internal static class ErrorHandlerSaverClass
+{
+    public static void HandleError(Exception e)
+    {
+        string day = DateTime.Now.DayOfYear.ToString();
+        string mills = DateTime.Now.Millisecond.ToString();
+        if (!Directory.Exists("ErrorFolder"))
+        {
+            Directory.CreateDirectory("ErrorFolder");
+        }
+        using (StreamWriter fs = new StreamWriter($"ErrorFolder/errorlog {day + " " + mills}.txt", false))
+        {
+            fs.Write(e.Message);
+        }
+        using (FileStream fs = new FileStream($"ErrorFolder/errorlog {day + " " + mills}.json", FileMode.OpenOrCreate))
+        {
+            List<string> list = new List<string>()
+                            {
+                                e.Message,
+                                e.StackTrace,
+                                e.InnerException?.ToString(),
+                                e.Source,
+                                e.TargetSite?.ToString(),
+                                e.HResult.ToString()
+                            };
+            JsonSerializer.Serialize(fs, list);
+        }
+    }
+}
+
---
+++ AvaloniaApplication3/Models/Wrapper_fractions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaApplication3.Models
+{
+    public class Wrapper_fractions
+    {
+        public List<string> term = new List<string>();
+        public Dictionary<string,Fraction> fraction_substitutions = new Dictionary<string,Fraction>();
+
+        public Wrapper_fractions(List<string> term, Dictionary<string, Fraction> fraction_substitutions)
+        {
+            this.term = term;
+            this.fraction_substitutions = fraction_substitutions;
+        }
+    }
+}
